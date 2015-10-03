64f5ae906d1cd363a5eeb33c24f122879af46cd8
Eduard Erja <eduard.erja@gmail.com>
2015-10-03 16:39:08 +0300

    Template tags
    Added template tags

--- home/views.py
+++ home/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import About
 
 def welcome_home(request):
-	about = About.getobjects.all()
+	about = About.objects.all()
 	return render(request, 'home/welcome_home.html', {'about': about})
