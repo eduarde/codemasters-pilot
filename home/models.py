e84c81b6e3df3cbed19b1a3bf760af89c8359b99
Eduard Erja <eduard.erja@gmail.com>
2015-10-03 20:50:31 +0300

    Trying to display avatar in template
    issues

--- home/models.py
+++ home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 class About(models.Model):
 	title = models.CharField(max_length=200)
@@ -7,12 +8,14 @@ class About(models.Model):
 	def __str__(self):
 		return self.title	
 
+
 class Member(models.Model):
 	first_name = models.CharField(max_length=50)
 	last_name = models.CharField(max_length=50)
 	position = models.CharField(max_length=200)
 	biography = models.TextField()
-	avatar = models.ImageField(upload_to='avatars_folder/')
+	avatar = models.ImageField(upload_to='static/images')
 
 	def __str__(self):
 		return self.last_name
+
--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import About
+from .models import About, Member
 
 def welcome_home(request):
 	about = About.objects.all()
-	return render(request, 'home/welcome_home.html', {'about': about})
+	members = Member.objects.all()
+	return render(request, 'home/welcome_home.html', {'about': about, 'members': members})
